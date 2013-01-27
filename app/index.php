fc5bbdf4effa18e2fa6a4ce23630bf0a87415a1a
mattkoskela <mattkoskela@gmail.com>
2013-01-26 17:43:15 -0800

    Added vote links and cleaned up index variable names a bit.

--- app/index.php
+++ app/index.php
@@ -4,26 +4,36 @@ require_once("config.php");
 require_once("db.php");
 
 echo "Home Page<br /><br />";
-    
-$topics = db_fetch("SELECT COUNT(topics.id) AS count, topics.id, topics.name FROM topics JOIN videos ON videos.topic_id = topics.id GROUP BY topics.id");
-foreach ($topics as $key => $topic) {
-    if ($topic["count"] <= CONFIG_MIN_VIDEO_COUNT) unset($topics[$key]);
+
+if (array_key_exists("topic_id", $_GET) && is_numeric($_GET["topic_id"])) {
+    //Topic id has been set
+    $topic_id = $_GET["topic_id"];
+    $topic = db_fetch("SELECT id, name FROM topics WHERE id = ?", array($topic_id), True);
+
+} else {
+
+    //Chose random topic
+    $topics = db_fetch("SELECT COUNT(topics.id) AS count, topics.id, topics.name FROM topics JOIN videos ON videos.topic_id = topics.id GROUP BY topics.id");
+    foreach ($topics as $key => $topic) {
+        if ($topic["count"] <= CONFIG_MIN_VIDEO_COUNT) unset($topics[$key]);
+    }
+    $topic_key = array_rand($topics);
+    $topic = $topics[$topic_key];
 }
-$topic_key = array_rand($topics);
-$random_topic = $topics[$topic_key];
-$topic_id = $random_topic["id"];
 
 echo "Topic:";
-echo "<pre>"; print_r($random_topic); echo "</pre>";
+echo "<pre>"; print_r($topic); echo "</pre>";
 
-$videos = db_fetch("SELECT * FROM videos WHERE topic_id = ?", array($topic_id));
+$videos = db_fetch("SELECT * FROM videos WHERE topic_id = ?", array($topic["id"]));
 $video_keys = array_rand($videos, 2);
-$video_id_1 = $videos[$video_keys[0]];
-$video_id_2 = $videos[$video_keys[1]];
+$video_1 = $videos[$video_keys[0]];
+$video_2 = $videos[$video_keys[1]];
 
 echo "Videos:";
-echo "<pre>"; print_r($video_id_1); echo "</pre>";
-echo "<pre>"; print_r($video_id_2); echo "</pre>";
+echo "<pre>"; print_r($video_1); echo "</pre>";
+echo "<pre>"; print_r($video_2); echo "</pre>";
 
+echo "<a href='vote.php?win=" . $video_1["id"] . "&lose=" . $video_2["id"] . "&topic_id=" . $topic["id"] . "'>Vote (" . $video_1["name"] . ")</a><br />";
+echo "<a href='vote.php?win=" . $video_2["id"] . "&lose=" . $video_1["id"] . "&topic_id=" . $topic["id"] . "'>Vote (" . $video_2["name"] . ")</a><br />";
 
 ?>
