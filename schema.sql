1987e80df6aca10c6c17609052264f07318d16fa
mattkoskela <mattkoskela@gmail.com>
2013-01-27 01:13:10 -0800

    Updated schema to have video.votes and removed dummy data now that the app is functioning.

--- schema.sql
+++ schema.sql
@@ -1,9 +1,11 @@
+CREATE DATABASE videowars;
+USE videowars;
 -- phpMyAdmin SQL Dump
 -- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 27, 2013 at 05:24 AM
+-- Generation Time: Jan 27, 2013 at 10:11 AM
 -- Server version: 5.5.25
 -- PHP Version: 5.4.4
 
@@ -25,23 +27,7 @@ CREATE TABLE `categories` (
   `name` varchar(512) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `name`, `timestamp`) VALUES
-(1, 'Aww', '2013-01-27 00:17:04'),
-(2, 'Sports', '2013-01-27 00:17:04'),
-(3, 'Fail', '2013-01-27 00:17:04'),
-(4, 'Comedy', '2013-01-27 00:17:04'),
-(5, 'Music', '2013-01-27 00:17:04'),
-(6, 'Politics', '2013-01-27 00:17:04'),
-(7, 'Other', '2013-01-27 00:17:04'),
-(8, 'WTF', '2013-01-27 00:17:04'),
-(9, 'Scary', '2013-01-27 00:17:04'),
-(10, 'Art', '2013-01-27 00:17:04');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -59,18 +45,7 @@ CREATE TABLE `topics` (
   `visibility` tinyint(1) unsigned NOT NULL DEFAULT '1',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `topics`
---
-
-INSERT INTO `topics` (`id`, `name`, `category_id`, `allow_skips`, `allow_additions`, `allow_leaderboard`, `visibility`, `created`) VALUES
-(1, 'Cats', 1, 1, 1, 1, 1, '2013-01-26 23:46:01'),
-(2, 'Superbowl Ads', 2, 1, 1, 1, 1, '2013-01-27 00:20:55'),
-(3, 'Best NBA Slam Dunks', 2, 1, 1, 1, 1, '2013-01-27 00:20:55'),
-(4, 'Best Rap Battle', 5, 1, 1, 1, 1, '2013-01-27 00:20:55'),
-(5, 'Best Guitar Solos', 5, 1, 1, 1, 1, '2013-01-27 00:20:55');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -83,29 +58,10 @@ CREATE TABLE `videos` (
   `youtube_id` varchar(256) NOT NULL,
   `topic_id` int(10) unsigned NOT NULL,
   `name` varchar(512) NOT NULL,
+  `votes` int(10) unsigned NOT NULL DEFAULT '0',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
-
---
--- Dumping data for table `videos`
---
-
-INSERT INTO `videos` (`id`, `youtube_id`, `topic_id`, `name`, `created`) VALUES
-(1, 'q1dpQKntj_w', 1, '10 Cutest Cat Moments', '2013-01-26 23:47:45'),
-(2, 'ctJJrBw7e-c', 1, 'Cats 1', '2013-01-27 00:28:00'),
-(3, 'J---aiyznGQ', 1, 'Cats 2', '2013-01-27 00:28:00'),
-(4, 'IytNBm8WA1c', 1, 'Cats 3', '2013-01-27 00:28:00'),
-(5, '2XID_W4neJo', 1, 'Cats 4', '2013-01-27 00:28:00'),
-(6, 'wf_IIbT8HGk', 1, 'Cats 5', '2013-01-27 00:28:00'),
-(7, 'NfCm9P8naDQ', 2, 'Superbowl 1', '2013-01-27 00:29:43'),
-(8, 'cidAbexlHTE', 2, 'Superbowl 2', '2013-01-27 00:29:43'),
-(9, 'PK9s6vVPCSo', 2, 'Superbowl 3', '2013-01-27 00:29:43'),
-(10, '6uFQAqwbwSg', 2, 'Superbowl 4', '2013-01-27 00:29:43'),
-(11, 'QAwnhlGud6w', 2, 'Superbowl 5', '2013-01-27 00:29:43'),
-(12, 'idlYCrgDjX4', 3, 'Dunks 1', '2013-01-27 00:31:00'),
-(13, '5BrkmIqUXck', 4, 'Rap 1', '2013-01-27 00:31:00'),
-(14, 'x_2tSGxGnJQ', 5, 'Guitar Solo 1', '2013-01-27 00:31:31');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -118,28 +74,7 @@ CREATE TABLE `views` (
   `video_id` int(10) unsigned NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
-
---
--- Dumping data for table `views`
---
-
-INSERT INTO `views` (`id`, `video_id`, `timestamp`) VALUES
-(1, 1, '2013-01-27 03:52:39'),
-(2, 2, '2013-01-27 03:52:39'),
-(3, 3, '2013-01-27 03:52:39'),
-(4, 4, '2013-01-27 03:52:39'),
-(5, 5, '2013-01-27 03:52:39'),
-(6, 6, '2013-01-27 03:52:39'),
-(7, 7, '2013-01-27 03:52:39'),
-(8, 8, '2013-01-27 03:52:39'),
-(9, 9, '2013-01-27 03:52:39'),
-(10, 10, '2013-01-27 03:52:39'),
-(11, 11, '2013-01-27 03:52:39'),
-(12, 12, '2013-01-27 03:52:39'),
-(13, 13, '2013-01-27 03:52:39'),
-(14, 14, '2013-01-27 03:52:39'),
-(15, 3, '2013-01-27 03:52:47');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -153,19 +88,5 @@ CREATE TABLE `votes` (
   `lose_video_id` int(10) unsigned NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `votes`
---
-
-INSERT INTO `votes` (`id`, `win_video_id`, `lose_video_id`, `timestamp`) VALUES
-(1, 1, 2, '2013-01-27 02:42:59'),
-(2, 1, 3, '2013-01-27 02:42:59'),
-(3, 1, 4, '2013-01-27 02:42:59'),
-(4, 2, 3, '2013-01-27 02:42:59'),
-(5, 2, 4, '2013-01-27 02:42:59'),
-(6, 2, 5, '2013-01-27 02:42:59'),
-(7, 3, 7, '2013-01-27 02:42:59'),
-(8, 7, 1, '2013-01-27 04:04:06'),
-(9, 8, 1, '2013-01-27 04:04:06');
